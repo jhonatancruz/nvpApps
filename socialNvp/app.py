b3736718a920ea35f0f2e3a3cb5876cfd1fdde81
Jhonatan Cruz <jcruz3@drew.edu>
2017-03-09 00:20:20 -0500

    new app configuration

--- socialNvp/app.py
+++ socialNvp/app.py
@@ -1,176 +1,136 @@
-from flask import Flask, render_template, request, session, redirect, g, url_for
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template,redirect, url_for, request
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, IntegerField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import os
-from flask.ext.login import LoginManager
-from datetime import datetime
-from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g
-from flask.ext.login import login_user , logout_user , current_user , login_required
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY']= "thisissecret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/nvp'
-db = SQLAlchemy(app)
-app.secret_key= os.urandom(24)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/imgs' #for uploads
+Bootstrap(app)
+db= SQLAlchemy(app)
+admin= Admin(app)
 login_manager= LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view= 'login'
 
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
 
-# Create our database model
-class Members(UserMixin, db.Model):
-    __tablename__ = "Members"
-    id = db.Column(db.Integer, primary_key=True)
-    firstName= db.Column(db.String(20))
-    lastName= db.Column(db.String(20))
-    company= db.Column(db.String(50))
+class User(UserMixin, db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(50))
+    lastName = db.Column(db.String(50))
+    email= db.Column(db.String(50), unique=True)
+    company = db.Column(db.String(50))
     phoneNumber= db.Column(db.VARCHAR(15),unique=True)
-    email = db.Column(db.String(120), unique=True)
+    password= db.Column(db.String(80))
+    img_url= db.Column(db.String(100))
     emailGroup= db.Column(db.String(120))
-    password= db.Column(db.VARCHAR(15), unique=True)
-    # registered_on= db.Column(db.DateTime)
-    is_active= db.Column(db.Boolean, default=False)
+    is_active= db.Column(db.Boolean, default=True)
     has_slack= db.Column(db.Boolean, default=False)
     has_kisi= db.Column(db.Boolean, default=False)
     has_agreement= db.Column(db.Boolean, default=False)
     has_mailbox= db.Column(db.Boolean, default=False)
     lockerNum= db.Column(db.VARCHAR(15),unique=True)
+    admin= db.Column(db.Boolean, default=False)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    def __init__(self, firstName, lastName, company, phoneNumber, email,password, is_active, has_slack,has_kisi, has_agreement,has_mailbox, lockerNum):
-        self.firstName = firstName
-        self.lastName= lastName
-        self.company= company
-        self.phoneNumber= phoneNumber
-        self.email= email
-        self.password= password
-        # self.registered_on= registered_on
-        self.is_active= is_active
-        self.has_slack= has_slack
-        self.has_kisi= has_kisi
-        self.has_agreement= has_agreement
-        self.has_mailbox= has_agreement
-        self.lockerNum= lockerNum
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Length(min=4, max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember= BooleanField('rememeber me')
 
-    def is_authenticated(self):
-        return True
+class RegisterForm(FlaskForm):
+    email= StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    firstName = StringField('First Name', validators=[InputRequired(), Length(min=4, max=25)])
+    lastName = StringField('Last Name', validators=[InputRequired(), Length(min=4, max=25)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    company = StringField('Company', validators=[InputRequired(), Length(min=8, max=25)])
+    phoneNumber= IntegerField("Phone Number")
 
-    def is_active(self):
-        return True
+admin.add_view(ModelView(User, db.session))
 
-    def is_anonymous(self):
-        return False
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    def get_id(self):
-        return (self.id)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    allForm= User()
 
-    def __repr__(self):
-        return '<User %r>' % (self.email)
+    if form.validate_on_submit():
+        user= User.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
 
-#--------------------------------------------------------------------------------------------#
-# landing page
-    @app.route('/')
-    def homepage():
-        return render_template('index.html')
+        return '<h1>Invalid email or password</h1>'
+        # return '<h1>' + form.username.data + ' '+ form.password.data + '</h1>'
 
-    #routes to the sign up page
-    @app.route('/SignUp.html', methods=['POST'])
-    def SignUp():
-        return render_template('SignUp.html')
+    return render_template('login.html', form=form, allForm=allForm)
 
-    @app.route('/registering', methods=['POST'])
-    def registering():
-        if request.method == 'POST':
-            email = request.form['email']
-            firstName= request.form['firstName']
-            lastName= request.form['lastName']
-            fullName= firstName+lastName
-            company = request.form['company']
-            phoneNumber= request.form['phoneNumber']
-            password= request.form['password']
-            # registered_on= db.DateTime
-            is_active= False
-            has_slack= False
-            has_kisi= False
-            has_agreement= False
-            has_mailbox= False
-            lockerNum= 0
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form= RegisterForm()
 
-
-            # Check that email does not already exist (not a great query, but works)
-            # if not db.session.query(Members.filter(Members.phoneNumber == phoneNumber).count():
-            user= Members(firstName, lastName, company, phoneNumber, email, password, is_active, has_slack, has_kisi, has_agreement, has_mailbox, lockerNum)
-            db.session.add(user)
-            db.session.commit()
-            return render_template('success.html')
-        return render_template('index.html')
-
-    #routes to the login page
-    @app.route('/login.html', methods=['POST'])
-    def login():
-        return render_template('login.html')
-
-    #verifies email and password for user login
-    # @app.route('/login', methods=['POST'])
-    # def loginVerify():
-    #     email= request.form['email']
-    #     password= request.form['password']
-    #     registered_user = Members.query.filter_by(email=email,password=password).first()
-    #     if request.method== 'POST':
-    #         if registered_user:
-    #             login_user(registered_user)
-    #             flash('Successful login')
-    #         else:
-    #              flash('Username or Password is invalid' , 'error')
-    #              return redirect(url_for('login'))
-    #     return render_template('login.html')
-    #
-    #         # if request.form['email']== 'jcruz3@drew.edu' & request.form['password']=='password123':
-    #         #     em= Members.query.filter_by(email= email).first()
-    #         #     pas= Members.query.filter_by(password= password).first()
-    #         #     login_user(em)
-    #         #     return render_template('profile.html', members=em)
-    #         # return render_template('error.html')
-
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        return redirect(url_for('homepage'))
-
-    @app.route('/profile')
-    @login_required
-    def profile():
-        if g.Members:
-            return render_template('profile.html')
+    if form.validate_on_submit():
+        filename = photos.save(request.files['photo'])
+        hash_password= generate_password_hash(form.password.data, method='sha256')
+        new_user= User(firstName= form.firstName.data,lastName= form.lastName.data,  email= form.email.data,company= form.company.data, phoneNumber= form.phoneNumber.data, password= hash_password, img_url= filename)
+        db.session.add(new_user)
+        db.session.commit()
         return redirect(url_for('login'))
+        #return '<h1> New user has been created</h1>'
+        # return '<h1>' + form.username.data + ' '+ form.password.data + ' '+ form.email.data+ '</h1>'
+    return render_template('signup.html', form= form)
 
-    # global variable for use
-    @app.before_request
-    def before_request():
-        g.Members= None
-        if 'Members' in session:
-            g.Members= session['Members']
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', firstName= current_user.firstName, img_url= current_user.img_url,
+                             lastName= current_user.lastName,  email= current_user.email, company= current_user.company,
+                             phoneNumber= current_user.phoneNumber, is_active= current_user.is_active, has_slack=current_user.has_slack,
+                             has_kisi=current_user.has_kisi, has_agreement= current_user.has_agreement, has_mailbox= current_user.has_mailbox,
+                             lockerNum= current_user.lockerNum)
+    # current_user.img_url= 'filename'
+    # #url= User(current_user.img_url= 'filename')
+    # db.session.add(current_user.img_url)
+    # db.session.commit()
+    # return '<h1>'+ current_user.email+'</h1>'
 
-    # @app.route('/getsession')
-    # def getsession():
-    #     if 'Members' in session:
-    #         return session['Members']
-    #     return 'Not Logged in'
-    #
-    # @app.route('/dropsession')
-    # def dropsession():
-    #     session.pop('Members', None)
-    #     return 'Dropped'
 
-    #routes to the admin dashboard
-    @app.route('/dashboard.html', methods=['POST', 'GET'])
-    @login_required
-    def dashboard():
-        members= Members.query.all()
-        user='jcruz3@drew.edu'
-        # members= db.session.query(Members).filter(Members.password).first()
-        return render_template('dashboard.html', members=members)
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
-    if __name__ == '__main__':
-        app.debug = True
-        app.run()
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        return redirect(url_for('dashboard'))
+    return render_template('upload.html')
+
+@app.route('/facebook', methods=['GET', 'POST'])
+def facebook():
+    users= User.query.all()
+    return render_template('facebook.html', users=users, img_url= current_user.img_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
